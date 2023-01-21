9741797b0d1d4384284ac011dec9eb2368707c16
Matthieu - Greep <42576124+GreepTheSheep@users.noreply.github.com>
2023-01-21 14:28:01 +0100

    First build

---
+++ assets/HomeView-da07e8c6.js
@@ -0,0 +1,1 @@
+import{_ as e,o as n,c as o}from"./index-6b025b73.js";const c={};function t(r,s){return n(),o("main",null," Hi, coming soon i think ")}const i=e(c,[["render",t]]);export{i as default};
